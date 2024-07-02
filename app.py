1c18ba4c63155d5649c1bbb09b9f73cda353bb3b
success-10 <successadako@gmail.com>
2024-07-02 21:39:11 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+import requests
+from config import OPENWEATHER_API_KEY, IPINFO_TOKEN
+
+
+app = Flask(__name__)
+
+@app.route('/api/hello', methods=['GET'])
+def hello():
+    visitor_name = request.args.get('visitor_name', 'Guest')
+    client_ip = request.remote_addr
+
+    geo_res = requests.get(f'https://ipinfo.io/{client_ip}/json?token={IPINFO_TOKEN}')
+    geo_data = geo_res.json()
+    location = geo_data.get("city") 
+    # print(f"Geo Data: {geo_data}")
+
+    # Use OpenWeather to get temperature
+    weather_res = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={location}&appid={OPENWEATHER_API_KEY}&units=metric')
+    weather_data = weather_res.json()
+    temperature = weather_data.get('main', {}).get('temp', 'Unknown')
+    
+    
+    response = {
+        "client_ip": client_ip,
+        "location": location,
+        "greeting": f"Hello, {visitor_name}! The temperature is {temperature} degrees Celsius in {location}"
+    }
+    return jsonify(response)
