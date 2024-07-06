45bf5bce869c544d7d684927858fa9131970185c
success-10 <successadako@gmail.com>
2024-07-06 16:36:40 +0100

    most recent

--- app.py
+++ app.py
@@ -8,12 +8,11 @@ app = Flask(__name__)
 @app.route('/api/hello', methods=['GET'])
 def hello():
     visitor_name = request.args.get('visitor_name', 'Guest')
-    client_ip = request.remote_addr
-
-    geo_request = requests.get(f'https://ipinfo.io/{client_ip}/json?token={IPINFO_TOKEN}')
+    
+    geo_request = requests.get(f'https://ipinfo.io?token={IPINFO_TOKEN}')
     geo_data = geo_request.json()
-    location = geo_data.get("city", "lagos")
-    # print(f"Geo Data: {geo_data}")
+    location = geo_data.get("city")
+    client_ip = geo_data.get("ip")
 
     # Use OpenWeather to get temperature
     weather_request = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={location}&appid={OPENWEATHER_API_KEY}&units=metric')
